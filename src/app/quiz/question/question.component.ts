f37276011dfed6c19bd96df7454a6ece592970fd
mario <mario.aboujamra@gmail.com>
2025-10-15 11:55:01 +0200

    fix: type issue

--- src/app/quiz/question/question.component.ts
+++ src/app/quiz/question/question.component.ts
@@ -1,25 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { QuizService } from "../../shared/services/quiz/quiz.service";
 import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-question',
-  templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss'],
-  standalone: false
+  selector: "app-question",
+  templateUrl: "./question.component.html",
+  styleUrls: ["./question.component.scss"],
+  standalone: false,
 })
 export class QuestionComponent implements OnInit {
-  quizContent: any[] = this.quizService.quizContent;
   id: number = 0;
 
-  constructor(private quizService: QuizService, private route: ActivatedRoute) { 
+  constructor(
+    private quizService: QuizService,
+    private route: ActivatedRoute,
+  ) {
     this.route.params.subscribe((params) => {
-      this.id = params["id"];
+      this.id = +params["id"];
     });
   }
 
   ngOnInit(): void {
-    this.quizContent = this.quizService.getQuizContentById(this.id);
+    this.quizService.resetQuiz();
+
+    this.quizService.getQuizContentById(this.id);
+  }
+
+  get quizContent(): any[] {
+    return this.quizService.quizContent;
   }
 
   addAnswer(answer: string, questionId: number) {
